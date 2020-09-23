bf015c001b27a3a2c35437022037197029f916b6
emanovic7 <e_owusu@live.com>
2020-09-23 10:00:01 -0400

    adds guessedWordsReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import guessedWordReducer from './guessedWordReducer';
 import successReducer from './successReducer';
+import guessedWords from './guessedWordReducer';
 
 
 export default combineReducers({
   success: successReducer,
+  guessedWords: guessedWordReducer,
 });
