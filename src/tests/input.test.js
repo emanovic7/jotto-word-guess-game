cc99568dfad328450e2ec8d097cea9afbf4ae3fd
emanovic7 <e_owusu@live.com>
2020-09-22 20:39:09 -0400

    adds storeFactory to testUtils, adds skeleton to input tests and component

---
+++ src/tests/input.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import  { findByTestAttr, storeFactory } from './testUtils';
+import Input from '../components/Input';
+
+const setupWrapper = (initialState={}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<Input store={store} />).dive().dive();
+  return wrapper;
+}
+
+describe('render', () => {
+  describe('word has not been guessed', () =>{
+    test('renders component without error', () => {
+
+    });
+    test('renders input box', () => {
+
+    });
+    test('renders submit button', () => {
+
+    });
+  })
+  describe('word has been guessed', () => {
+    test('renders component without error', () => {
+
+    });
+    test('does not render input box', () => {
+
+    });
+    test('does not render submit button', () => {
+
+    });
+  });
+});
+
+
+describe('update state', () => {
+
+});
