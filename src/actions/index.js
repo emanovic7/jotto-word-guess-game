a42e9c899b8ebbb984ee2804fecdb4dcc2653590
emanovic7 <e_owusu@live.com>
2020-09-23 18:32:05 -0400

    adds axios, moxios, sets up tests for actions/index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+
 import { getLetterMatchCount } from '../helpers';
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD',
 };
 
 /**
@@ -27,3 +30,10 @@ export const guessWord = (guessedWord) => {
     }
   };
 };
+
+
+export const getSecretWord = () => {
+  return (dispatch) => {
+    axios.get('http://localhost:3030');
+  }
+}
