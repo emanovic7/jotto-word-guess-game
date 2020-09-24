b9afeebfe410a5ff6a979f886c09f19b8bc87c6c
emanovic7 <e_owusu@live.com>
2020-09-24 16:16:25 -0400

    adds guessWord input tests, adds beforeEach

--- src/tests/input.test.js
+++ src/tests/input.test.js
@@ -68,19 +68,34 @@ describe('redux props', () => {
 });
 
 describe('`guessWord` action creator call', () => {
-  test('calls `guessWord` when button is clicked', () => {
-    const guessWordMock = jest.fn();
+  let guessWordMock;
+  let wrapper;
+  const guessedWord = 'train';
+  beforeEach(() => {
+    guessWordMock = jest.fn();
+
     const props = {
       success: false,
       guessWord: guessWordMock,
     };
+
     //set up app component with guessWordMock as the guessWord prop
-    const wrapper = shallow(<UnconnectedInput {...props} />)
+    wrapper = shallow(<UnconnectedInput {...props} />);
+
+    //add value to input box
+    wrapper.setState({ currentGuess: guessedWord })
+
     //simulate click
     const submitButton = findByTestAttr(wrapper, 'submit-button');
     submitButton.simulate('click');
+  });
+  test('calls `guessWord` when button is clicked', () => {
     //check to see if mock ran
     const guessWordCallCount = guessWordMock.mock.calls.length;
     expect(guessWordCallCount).toBe(1);
+  });
+  test('calls `guessWord` with input value as argument', () => {
+    const guessWordArg = guessWordMock.mock.calls[0][0];
+    expect(guessWordArg).toBe(guessedWord);
   });
 });
